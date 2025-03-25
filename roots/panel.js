9188249355271b9186286e7101f0c54b14cd24ee
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-03-25 19:03:35 +0100

    add verify req origin for panel roots

--- roots/panel.js
+++ roots/panel.js
@@ -23,6 +23,7 @@ router.use(expressLayouts);
 
 router.get('/', async (req, res) => {
 
+  if (req.hostname === config.hostname) {
 
       const token = req.cookies.silvertoken;
   
@@ -62,20 +63,48 @@ router.get('/', async (req, res) => {
   
       })
 
+    } else {
+      return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+    };
+
   });
   
 
 
 router.get('/admin/users', (req, res) => {
-  res.render('panel/admin/users');
+
+  if (req.hostname === config.hostname) {
+
+    res.render('panel/admin/users');
+
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
+
 });
 
 router.get('/admin/apikey', (req, res) => {
-  res.render('panel/admin/apikey');
+
+  if (req.hostname === config.hostname) {
+
+    res.render('panel/admin/apikey');
+
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
+
 });
 
 router.get('/admin', (req, res) => {
-  res.render('panel/admin/admin');
+
+  if (req.hostname === config.hostname) {
+
+    res.render('panel/admin/admin');
+
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
+
 });
 
 
@@ -84,134 +113,164 @@ router.get('/admin', (req, res) => {
 
 router.get('/apikey', async (req, res) => {
 
-  const token = req.cookies.silvertoken;
+  if (req.hostname === config.hostname) {
 
-  await Token.verify(token).then(async (resp) => {
+    const token = req.cookies.silvertoken;
 
-    if (resp.valid === true) {
+    await Token.verify(token).then(async (resp) => {
 
-      res.render('panel/apikey');
+      if (resp.valid === true) {
 
-    }
+        res.render('panel/apikey');
 
-    else {
-      res.redirect('/auth/view/login?redirect=/panel')
-    }
+      }
 
-  })
+      else {
+        res.redirect('/auth/view/login?redirect=/panel')
+      }
+
+    })
+
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 });
 
 
 router.get('/apikey/data', async (req, res) => {
 
-  const token = req.cookies.silvertoken;
+  if (req.hostname === config.hostname) {
 
-  await Token.verify(token).then(async (resp) => {
+    const token = req.cookies.silvertoken;
 
-    if (resp.valid === true) {
+    await Token.verify(token).then(async (resp) => {
 
-      await apikey.GetKeys(resp.data.usr_info.userId).then(respd => {
+      if (resp.valid === true) {
 
-        res.json(respd);
+        await apikey.GetKeys(resp.data.usr_info.userId).then(respd => {
 
-      })
+          res.json(respd);
 
-    }
+        })
 
-    else {
-     return { error: true, message: 'Session invalide' }
-    }
+      }
 
-  })
+      else {
+      return { error: true, message: 'Session invalide' }
+      }
+
+    })
+
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 })
 
 
 router.get('/apikey/data/admin', async (req, res) => {
 
-  const token = req.cookies.silvertoken;
+  if (req.hostname === config.hostname) {
 
-  await Token.verify(token).then(async (resp) => {
+    const token = req.cookies.silvertoken;
 
-    if (resp.valid === true) {
+    await Token.verify(token).then(async (resp) => {
 
-      if (resp.data.usr_info.dataplus.role === 'ADMIN') {
+      if (resp.valid === true) {
 
-        await apikey.GetKeys(resp.data.usr_info.dataplus.role).then(respd => {
+        if (resp.data.usr_info.dataplus.role === 'ADMIN') {
 
-          res.json(respd);
-  
-        })
+          await apikey.GetKeys(resp.data.usr_info.dataplus.role).then(respd => {
+
+            res.json(respd);
+    
+          })
+
+        }
 
       }
 
-    }
+      else {
+      return { error: true, message: 'Session invalide' }
+      }
 
-    else {
-     return { error: true, message: 'Session invalide' }
-    }
+    })
 
-  })
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 })
 
 
 router.get('/users/data/', async (req, res) => {
 
-  const token = req.cookies.silvertoken;
+  if (req.hostname === config.hostname) {
 
-  await Token.verify(token).then(async (resp) => {
+    const token = req.cookies.silvertoken;
 
-    if (resp.valid === true) {
+    await Token.verify(token).then(async (resp) => {
 
-      if (resp.data.usr_info.dataplus.role === 'ADMIN') {
+      if (resp.valid === true) {
 
-        await account.GetAccounts('ADMIN').then(respd => {
+        if (resp.data.usr_info.dataplus.role === 'ADMIN') {
 
-          res.json(respd);
-  
-        })
+          await account.GetAccounts('ADMIN').then(respd => {
+
+            res.json(respd);
+    
+          })
+
+        }
 
       }
 
-    }
+      else {
+      return { error: true, message: 'Session invalide' }
+      }
 
-    else {
-     return { error: true, message: 'Session invalide' }
-    }
+    })
 
-  })
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 })
 
 
 router.get('/users/update/:usrid', async (req, res) => { // voir la root + ajouter verification de toutes les roots intern et ajouter vérification de domaine
 
-  const usrID = req.params.usrid;
-  const token = req.cookies.silvertoken;
+  if (req.hostname === config.hostname) {
 
-  await Token.verify(token).then(async (resp) => {
+    const usrID = req.params.usrid;
+    const token = req.cookies.silvertoken;
 
-    if (resp.valid === true) {
+    await Token.verify(token).then(async (resp) => {
 
-      if (resp.data.usr_info.dataplus.role === 'ADMIN') {
+      if (resp.valid === true) {
 
-        await account.GetAccounts('ADMIN').then(respd => {
+        if (resp.data.usr_info.dataplus.role === 'ADMIN') {
 
-          res.json(respd);
-  
-        })
+          await account.GetAccounts('ADMIN').then(respd => {
+
+            res.json(respd);
+    
+          })
+
+        }
 
       }
 
-    }
+      else {
+      return { error: true, message: 'Session invalide' }
+      }
 
-    else {
-     return { error: true, message: 'Session invalide' }
-    }
+    })
 
-  })
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 })
 
@@ -220,30 +279,36 @@ router.get('/users/update/:usrid', async (req, res) => { // voir la root + ajout
 
 router.get('/users/data/:id', async (req, res) => {
 
-  const id = req.params.id;
-  const token = req.cookies.silvertoken;
+  if (req.hostname === config.hostname) {
 
-  await Token.verify(token).then(async (resp) => {
+    const id = req.params.id;
+    const token = req.cookies.silvertoken;
 
-    if (resp.valid === true) {
+    await Token.verify(token).then(async (resp) => {
 
-      if (resp.data.usr_info.dataplus.role === 'ADMIN') {
+      if (resp.valid === true) {
 
-        await account.GetAccounts(id).then(respd => {
+        if (resp.data.usr_info.dataplus.role === 'ADMIN') {
 
-          res.json(respd);
-  
-        })
+          await account.GetAccounts(id).then(respd => {
+
+            res.json(respd);
+    
+          })
+
+        }
 
       }
 
-    }
+      else {
+      return { error: true, message: 'Session invalide' }
+      }
 
-    else {
-     return { error: true, message: 'Session invalide' }
-    }
+    })
 
-  })
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 })
 
@@ -253,39 +318,45 @@ router.get('/users/data/:id', async (req, res) => {
 
 router.post('/apikey/key/maj/:key', async (req, res) => {
 
-  try {
+  if (req.hostname === config.hostname) {
+    
+    try {
 
-      const body = req.body;
-      const apiKey = req.params.key;
-      const token = req.cookies.silvertoken;
+        const body = req.body;
+        const apiKey = req.params.key;
+        const token = req.cookies.silvertoken;
 
-      if (req.hostname !== config.hostname) {
-          return res.status(403).json({ error: true, message: { silver: "Accès refusé" } });
-      }
+        if (req.hostname !== config.hostname) {
+            return res.status(403).json({ error: true, message: { silver: "Accès refusé" } });
+        }
 
-      const tokenResp = await Token.verify(token);
-      if (!tokenResp.valid) {
-          return res.status(401).json({ error: true, message: { silver: "Token invalide" } });
-      }
+        const tokenResp = await Token.verify(token);
+        if (!tokenResp.valid) {
+            return res.status(401).json({ error: true, message: { silver: "Token invalide" } });
+        }
 
-      const updateResp = await apikey.update(apiKey, {
-          organizationName: body.Name,
-          allowedDomains: body.Domaines,
-          redirectUrls: body.Redirects
-      });
+        const updateResp = await apikey.update(apiKey, {
+            organizationName: body.Name,
+            allowedDomains: body.Domaines,
+            redirectUrls: body.Redirects
+        });
 
-      if (updateResp.error) {
-          return res.status(500).json({ error: true, message: { silver: updateResp.message } });
-      }
+        if (updateResp.error) {
+            return res.status(500).json({ error: true, message: { silver: updateResp.message } });
+        }
 
-      return res.status(200).json({ message: { silver: "Mise à jour réussie" }, data: body });
+        return res.status(200).json({ message: { silver: "Mise à jour réussie" }, data: body });
 
-  } catch (error) {
+    } catch (error) {
 
-      console.error('Erreur APIKey update:', error);
-      return res.status(500).json({ error: true, message: { silver: "Erreur serveur" } });
+        console.error('Erreur APIKey update:', error);
+        return res.status(500).json({ error: true, message: { silver: "Erreur serveur" } });
 
-  }
+    }
+
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 });
 
@@ -314,77 +385,89 @@ function handleBigInt(obj) {
 
 router.post('/apikey/key/add', async (req, res) => {
 
-  try {
+  if (req.hostname === config.hostname) {
 
-    const organisationName = req.query.name;
-    const token = req.cookies.silvertoken;
+    try {
 
-    if (req.hostname !== config.hostname) {
-        return res.status(403).json({ error: true, message: { silver: "Accès refusé" } });
-    }
+      const organisationName = req.query.name;
+      const token = req.cookies.silvertoken;
 
-    const tokenResp = await Token.verify(token);
-    if (!tokenResp.valid) {
-        return res.status(401).json({ error: true, message: { silver: "Token invalide" } });
-    }
-
-    await apikey.create(tokenResp.data.usr_info.userId, organisationName ).then(resp => {
-
-      if (resp.error) {
-        return res.status(400).json({ error: true, message: { silver: 'Une erreur est survenue', server: resp.message }, data: resp });
+      if (req.hostname !== config.hostname) {
+          return res.status(403).json({ error: true, message: { silver: "Accès refusé" } });
       }
-      else {
-        return res.status(200).json({ message: 'Clé crée avec success', data: resp });
-      };
 
-    })
+      const tokenResp = await Token.verify(token);
+      if (!tokenResp.valid) {
+          return res.status(401).json({ error: true, message: { silver: "Token invalide" } });
+      }
+
+      await apikey.create(tokenResp.data.usr_info.userId, organisationName ).then(resp => {
+
+        if (resp.error) {
+          return res.status(400).json({ error: true, message: { silver: 'Une erreur est survenue', server: resp.message }, data: resp });
+        }
+        else {
+          return res.status(200).json({ message: 'Clé crée avec success', data: resp });
+        };
+
+      })
 
 
-  } catch (error) {
+    } catch (error) {
 
-    console.error('Erreur APIKey update:', error);
-    return res.status(500).json({ error: true, message: { silver: "Erreur serveur" } });
+      console.error('Erreur APIKey update:', error);
+      return res.status(500).json({ error: true, message: { silver: "Erreur serveur" } });
 
-  }
+    }
+
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 })
 
 router.post('/apikey/key/remove/:key', async (req, res) => {
 
-  try {
+  if (req.hostname === config.hostname) {
 
-    const apikey = req.params.key;
-    const token = req.cookies.silvertoken;
+    try {
 
-    // Vérification du domaine
-    if (req.hostname !== config.hostname) {
-        return res.status(403).json({ error: true, message: { silver: "Accès refusé" } });
+      const apikey = req.params.key;
+      const token = req.cookies.silvertoken;
+
+      // Vérification du domaine
+      if (req.hostname !== config.hostname) {
+          return res.status(403).json({ error: true, message: { silver: "Accès refusé" } });
+      }
+
+      // Vérification du token
+      const tokenResp = await Token.verify(token);
+      if (!tokenResp.valid) {
+          return res.status(401).json({ error: true, message: { silver: "Token invalide" } });
+      }
+
+      // Appel à la méthode delete
+      const resp = await deleteAPIKEY(apikey);
+
+
+      if (resp.error) {
+        return res.status(400).json({ error: true, message: { silver: 'Une erreur est survenue', server: resp.message }, data: resp });
+      }
+
+      // Si la suppression est réussie
+      return res.status(200).json({ message: resp.message, data: resp });
+
+    } catch (error) {
+
+      // Gestion d'erreur serveur
+      console.error('Erreur lors de la suppression de la clé API:', error);
+      return res.status(500).json({ error: true, message: { silver: "Erreur serveur" } });
+
     }
 
-    // Vérification du token
-    const tokenResp = await Token.verify(token);
-    if (!tokenResp.valid) {
-        return res.status(401).json({ error: true, message: { silver: "Token invalide" } });
-    }
-
-    // Appel à la méthode delete
-    const resp = await deleteAPIKEY(apikey);
-
-
-    if (resp.error) {
-      return res.status(400).json({ error: true, message: { silver: 'Une erreur est survenue', server: resp.message }, data: resp });
-    }
-
-    // Si la suppression est réussie
-    return res.status(200).json({ message: resp.message, data: resp });
-
-  } catch (error) {
-
-    // Gestion d'erreur serveur
-    console.error('Erreur lors de la suppression de la clé API:', error);
-    return res.status(500).json({ error: true, message: { silver: "Erreur serveur" } });
-
-  }
+  } else {
+    return res.send('Vous essayer d\'acceder a SilverAuth d\'une mauvaise manière !');
+  };
 
 });
 
